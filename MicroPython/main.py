ac5d11062e609e287c884a1efa20d887888b43fa
Ashlyn-Anpalahan <ashlyn.anpalahan@stu.ocsb.ca>
2026-03-02 21:54:09 +0000

    Code in python

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,9 +1,14 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
-This module is a Micro:bit MicroPython program
+Created by: Ashlyn
+Created on: Feb 2026
+This module will solve basic math
 """
 
 from microbit import *
 
-display.scroll("Hello, World!")
+display.clear()
+sleep(1)
+
+display.scroll("A rectangle has dimensions 5 cm & 3 cm")
+display.scroll("The perimeter would be" + 2 * (5 + 3) + " cm")
+display.scroll("The area would be" + (5 * 3) + " cm^2")
